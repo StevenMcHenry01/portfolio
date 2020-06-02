7040b05ae7655e512768566be378d3afdd4b44ef
Steven McHenry <stevenmchenry01@gmail.com>
2020-06-01 18:50:03 -0700

    auto

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import styled from 'styled-components'
 import { Card, Box, Container, useTheme } from '@material-ui/core'
-import Skills from '../components/Skills'
+// import Skills from '../components/Skills'
 import SEO from '../components/SEO'
+import { CenteredDiv } from '../styles/CenteredDiv'
+import Loading from '../components/Loading'
+
+const Skills = React.lazy(() => import('../components/Skills'))
 
 const SkillsPage = () => {
   const theme = useTheme()
@@ -12,20 +16,32 @@ const SkillsPage = () => {
     min-height: 100vh;
   `
   const ContainerStyled = styled(Container)`
-  display: flex;
-  margin-left: 0;
+    display: flex;
+    margin-left: 0;
   `
 
   return (
     <StyledPage>
       <SEO title='Skills' />
       <ContainerStyled maxWidth='lg'>
-      <Card>
-        <div style={{backgroundColor: theme.palette.switchable.skillsBackground ,padding: '30px'}}>
-
-        <Skills />
-        </div>
-      </Card>
+        <Suspense
+          fallback={
+            <CenteredDiv>
+              <Loading />
+            </CenteredDiv>
+          }
+        >
+          <Card>
+            <div
+              style={{
+                backgroundColor: theme.palette.switchable.skillsBackground,
+                padding: '30px',
+              }}
+            >
+              <Skills />
+            </div>
+          </Card>
+        </Suspense>
       </ContainerStyled>
     </StyledPage>
   )
