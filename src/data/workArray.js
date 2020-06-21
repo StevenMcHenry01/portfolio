8055f85fb0de5803435defb039cd63b0142c9419
Steven McHenry <stevenmchenry01@gmail.com>
2020-06-20 19:12:02 -0700

    auto

--- src/data/workArray.js
+++ src/data/workArray.js
@@ -1,17 +1,29 @@
 import React from 'react'
 
-import { FaReact, FaNode, FaFire, FaSitemap } from 'react-icons/fa'
+import {
+  FaReact,
+  FaNode,
+  FaFire,
+  FaSitemap,
+  FaUserShield,
+  FaFileCode,
+  FaUserSecret,
+} from 'react-icons/fa'
 
 export const workArray = [
   {
-    title: `Starcraft Ladder Battle`,
-    description: `React application calling a third party Api and caching information.`,
-    imageName: 'starcraft',
-    githubUrl: 'https://github.com/StevenMcHenry01/starcraft-ladder-client',
-    siteUrl: 'https://starcraft-ladder.netlify.app/',
+    title: `Reddit Royale`,
+    description: `A full-stack react application which implements a custom oauth flow and interacts with the reddit API to offer a seamless user experience.`,
+    imageName: 'reddit',
+    githubUrl: 'https://github.com/StevenMcHenry01/Reddit_Royale_Client',
+    siteUrl: 'https://reddit-royale.netlify.app',
     chipArray: [
       { key: 0, label: 'React', icon: <FaReact /> },
       { key: 1, label: 'Node', icon: <FaNode /> },
+      { key: 2, label: 'Express', icon: <FaSitemap /> },
+      { key: 3, label: 'Oauth', icon: <FaUserShield /> },
+      { key: 4, label: 'JWT', icon: <FaUserSecret /> },
+      { key: 5, label: 'Material UI', icon: <FaFileCode /> },
     ],
   },
   {
@@ -22,19 +34,21 @@ export const workArray = [
     locations.`,
     imageName: 'locations',
     githubUrl: 'https://github.com/StevenMcHenry01/locations',
-    siteUrl: 'https://locations-app.netlify.app/',
+    siteUrl: 'https://locations-app.netlify.app',
     chipArray: [
       { key: 0, label: 'React', icon: <FaReact /> },
       { key: 1, label: 'Node', icon: <FaNode /> },
       { key: 2, label: 'Express', icon: <FaSitemap /> },
+      { key: 3, label: 'JWT', icon: <FaUserSecret /> },
+      { key: 4, label: 'Material UI', icon: <FaFileCode /> },
     ],
   },
   {
     title: `Bob's Burgers Menu`,
-    description: `React application with auth and data preservaton through firebase.`,
+    description: `React application with auth and data preservaton through firebase. Showcases snappy UI that is translated across entire app instantly.`,
     imageName: 'bobs',
     githubUrl: 'https://github.com/StevenMcHenry01/bobs_burgers',
-    siteUrl: 'https://bobsburgers.netlify.app/',
+    siteUrl: 'https://bobsburgers.netlify.app',
     chipArray: [
       { key: 0, label: 'React', icon: <FaReact /> },
       { key: 1, label: 'Node', icon: <FaNode /> },
